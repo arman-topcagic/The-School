a94c3c2528a53d638d16272a1604aabb8e071c20
Arman Topcagic <arman.topcagic@elev.ga.ntig.se>
2025-09-19 11:26:50 +0200

    Added the option to edit Administrators variables.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
         //---- Object Info ----//
